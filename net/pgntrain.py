f37453eaaaa502b3079b3d935b29adf2948f2875
Michael Bieniosek <michael.bieniosek@redfin.com>
2024-12-30 11:42:57 -0800

    add c training system

--- net/pgntrain.py
+++ net/pgntrain.py
@@ -1,6 +1,7 @@
 import sys
 import chess.pgn
 import chessdecode
+import ctypes
 import numpy as np
 import pandas as pd
 import keras.utils
@@ -9,6 +10,17 @@ from keras import layers
 import random
 import cProfile, pstats, io
 from pstats import SortKey
+
+class TrainingPosition(ctypes.Structure):
+    # PNBRQKpnbrqk
+    _fields_ = [
+        ('piece_bitmasks', ctypes.c_uint8 * (12 * 8)),
+        ('piece_bitmasks_mirrored', ctypes.c_uint8 * (12 * 8)),
+        ('white_king_index', ctypes.c_int),
+        ('black_king_index_mirrored', ctypes.c_int),
+        ('cp_eval', ctypes.c_float),
+        ('padding', ctypes.c_float),
+    ]
 
 def mirror_vertical(board):
     return np.concatenate([board[(8*(7-i)):(8*(7-i))+8] for i in range(8)])
@@ -42,9 +54,14 @@ def get_positions(pgnfile):
 
 
 
+
 class NNUEModel:
 
-    def __init__(self, num_hidden_layers=2, hidden_layers_width=32, relu_fn=relu_sat):
+    TrainLib = ctypes.cdll.LoadLibrary('trainlib.dylib')
+    # https://github.com/official-stockfish/nnue-pytorch/blob/master/docs/nnue.md#loss-functions-and-how-to-apply-them
+    CP_WDL_SCALING_FACTOR = 410
+
+    def __init__(self, num_hidden_layers=2, hidden_layers_width=32, relu_fn=relu_sat, centipawn_output=True, wdl_output=True):
         self.CASTLE_KING_IDX = {'k': 32, 'q': 33, 'kq': 34, 'qk': 34}
         self.num_hidden_layers = num_hidden_layers
         self.hidden_layers_width = hidden_layers_width
@@ -54,6 +71,8 @@ class NNUEModel:
         self.black_piece_list = self._piece_list().lower() + self._piece_list().upper()
         self.INPUT_LENGTH = self._num_king_buckets() * len(self.white_piece_list) * 64
         self.EMPTY = np.zeros(self.INPUT_LENGTH, dtype=np.ubyte)
+        self.centipawn_output = centipawn_output
+        self.wdl_output = wdl_output
 
     def _num_king_buckets(self):
         return 35
@@ -102,9 +121,73 @@ class NNUEModel:
             theirboard = self.EMPTY.copy()
             for king_idx, present_board, castle_rights, container in ((w_king_idx, w_full, w_castle, myboard), (b_king_idx, b_full, b_castle, theirboard)):
                 king_idx, present_board = self._king_idx_map(present_board, castle_rights, king_idx)
-                container[king_idx * board_step_size:(king_idx + 1) * board_step_size] = np.concatenate(present_board)
+                container[king_idx * board_step_size:(king_idx + 1) * board_step_size] = np.concatenate(present_board).copy()
 
-            yield (np.array([myboard]), np.array([theirboard])), (np.array([keras.utils.to_categorical(int(np.sign(white_score.cp)) + 1, 3)]).astype(np.ubyte), np.array([white_score.cp]).astype(np.int16))
+            for x in self.to_instance_format(myboard, theirboard, white_score.cp):
+                yield x
+
+    def to_instance_format(self, myboard, theirboard, cp_eval):
+        if self.centipawn_output and self.wdl_output:
+            yield (np.array([myboard]), np.array([theirboard])), (np.array([keras.utils.to_categorical(int(np.sign(cp_eval)) + 1, 3)]).astype(np.ubyte), np.array([cp_eval]).astype(np.int16))
+        elif self.wdl_output:
+            win = 1/(1+(np.exp((-cp_eval/self.CP_WDL_SCALING_FACTOR))))
+            yield (np.array([myboard]), np.array([theirboard])), (np.array([[1-win, 0, win]]),)
+        elif self.centipawn_output:
+            yield (np.array([myboard]), np.array([theirboard])), (np.array([cp_eval]).astype(np.int16),)
+
+    def to_instance_batch_format(self, myboards, theirboards, cp_evals):
+        if len(myboards) == 0:
+            return
+        xfeat = (np.array(myboards), np.array(theirboards))
+        if self.centipawn_output and self.wdl_output:
+            yield xfeat, (keras.utils.to_categorical(np.sign(cp_evals) + 1, 3).astype(np.ubyte), np.array(cp_evals).astype(np.int16))
+        elif self.wdl_output:
+            win = 1/(1+(np.exp((-np.array(cp_evals)/self.CP_WDL_SCALING_FACTOR))))
+            yield xfeat, (np.array([1-win, np.zeros_like(win), win]).T,)
+        elif self.centipawn_output:
+            yield xfeat, (np.array(cp_evals).astype(np.int16),)
+
+    def fast_result_iterator(self, pgn_filename, batch_size):
+        board_step_size = 64 * len(self.white_piece_list)
+        tp = TrainingPosition()
+        self.TrainLib.create_training_iterator.restype = ctypes.c_void_p
+        self.TrainLib.create_training_iterator.argtypes = (ctypes.c_char_p, ctypes.c_int)
+        self.TrainLib.read_position.argtypes = (ctypes.c_void_p, ctypes.POINTER(TrainingPosition))
+        self.TrainLib.delete_training_iterator.argtypes = (ctypes.c_void_p, )
+
+        iter = self.TrainLib.create_training_iterator(pgn_filename.encode('utf-8'), 10)
+        has_more = 1
+        cp_evals = []
+        myboards = []
+        theirboards = []
+        while has_more:
+            has_more = self.TrainLib.read_position(iter, ctypes.byref(tp))
+            if has_more:
+                feat = np.unpackbits(np.array(tp.piece_bitmasks, dtype=np.ubyte))
+                myboard = self.EMPTY.copy()
+                theirboard = self.EMPTY.copy()
+
+                for king_idx, present_board, container in ((tp.white_king_index, tp.piece_bitmasks, myboard), (tp.black_king_index_mirrored, tp.piece_bitmasks_mirrored, theirboard)):
+                    king_idx, present_board = self._king_idx_map(present_board, '', king_idx)
+                    if len(self.white_piece_list) == 12:
+                        container[king_idx * board_step_size:(king_idx + 1) * board_step_size] = np.unpackbits(np.array([present_board[i] for i in range(12*8)], dtype=np.ubyte))
+                    elif len(self.white_piece_list) == 10:
+                        container[king_idx * board_step_size:(king_idx + 1) * board_step_size] = np.unpackbits(np.array([present_board[i] for i in itertools.chain(range(5*8), range(6*8, 11*8))], dtype=np.ubyte))
+                    else:
+                        raise Exception("Unsupported")
+                myboards.append(myboard)
+                theirboards.append(theirboard)
+                cp_evals.append(tp.cp_eval)
+
+                if len(myboards) >= batch_size:
+                    for x in self.to_instance_batch_format(myboards, theirboards, cp_evals):
+                        yield x
+                    myboards = []
+                    theirboards = []
+                    cp_evals = []
+        for x in self.to_instance_batch_format(myboards, theirboards, cp_evals):
+            yield x
+        self.TrainLib.delete_training_iterator(iter)
 
 
     def make_nnue_model_mirror(self, num_classes=3, include_centipawns=False):
@@ -118,39 +201,51 @@ class NNUEModel:
         x = layers.concatenate(hidden)
         for i in range(self.num_hidden_layers):
             x = layers.Dense(self.hidden_layers_width, activation=self.relu_fn)(x)
-        result = layers.Dense(num_classes, activation="softmax", name="result")(x)
-        if include_centipawns:
+        outputs = []
+        metrics = []
+        losses = []
+        loss_weights = []
+        if self.centipawn_output:
             centipawns = layers.Dense(1, name="centipawns")(x)
-            outputs = [result, centipawns]
-        else:
-            outputs = result
+            outputs.append(centipawns)
+            metrics.append("mean_squared_error")
+            losses.append("mean_squared_error")
+            loss_weights.append(.000002)
+        if self.wdl_output:
+            result = layers.Dense(num_classes, activation="softmax", name="wdl")(x)
+            outputs.append(result)
+            metrics.append("categorical_accuracy")
+            losses.append("categorical_crossentropy")
+            loss_weights.append(1)
 
-        model = keras.Model(inputs=tuple(inputs), outputs=outputs, name="lobsternet_nnue")
-        if include_centipawns:
-            model.compile(
-                loss=["categorical_crossentropy", "mean_squared_error"],
-                loss_weights=[1, .000002],
-                optimizer="adam",
-                metrics=["categorical_accuracy", "mean_squared_error"])
-        else:
-            model.compile(loss=["categorical_crossentropy"], optimizer="adam", metrics=["categorical_accuracy"])
+        model = keras.Model(inputs=tuple(inputs), outputs=tuple(outputs), name="lobsternet_nnue")
+        model.compile(
+            loss=losses,
+            loss_weights=loss_weights,
+            optimizer="adam",
+            metrics=metrics)
         return model
 
-    def train(self, train_pgn, valid_pgn, profile=False, **fit_args):
+    def train(self, train_pgn, valid_pgn, profile=False, batch_size=128, steps_per_epoch=256, **fit_args):
         model = self.make_nnue_model_mirror(include_centipawns=True)
         model.summary()
+        output_sig = []
+        if self.centipawn_output:
+            output_sig.append(tf.TensorSpec(shape=(None, ), dtype=tf.int16, name='centipawns'))
+        if self.wdl_output:
+            output_sig.append(tf.TensorSpec(shape=(None, 3), dtype=tf.float32, name='result'))
+
         sign = ((tf.TensorSpec(shape=(None, self.INPUT_LENGTH), dtype=tf.uint8, name='side_0'),
                 tf.TensorSpec(shape=(None, self.INPUT_LENGTH), dtype=tf.uint8, name='side_1')),
-                (tf.TensorSpec(shape=(None, 3), dtype=tf.uint8, name='result'),
-                tf.TensorSpec(shape=(None, ), dtype=tf.int16, name='centipawns')))
-        tf_data_generator = tf.data.Dataset.from_generator(lambda: self.centipawn_result_iterator(get_positions(train_pgn)),
+                tuple(output_sig))
+        tf_data_generator = tf.data.Dataset.from_generator(lambda: self.fast_result_iterator(train_pgn, batch_size=batch_size),
             output_signature=sign)
         validation_data_generator = tf.data.Dataset.from_generator(lambda: self.centipawn_result_iterator(get_positions(valid_pgn)),
             output_signature=sign)
         if profile:
             pr = cProfile.Profile()
             pr.enable()
-        history = model.fit(tf_data_generator, validation_data=validation_data_generator, batch_size=128, steps_per_epoch=256, **fit_args)
+        history = model.fit(tf_data_generator, validation_data=validation_data_generator, batch_size=batch_size, steps_per_epoch=steps_per_epoch, **fit_args)
         if profile:
             pr.disable()
             pr.dump_stats('out.stats')
@@ -161,8 +256,8 @@ def sqrelu_clamp(x):
     return tf.square(keras.activations.relu(x, max_value=1))
 
 class BasicNNUE(NNUEModel):
-    def __init__(self, num_hidden_layers=0, hidden_layers_width=32):
-        super().__init__(num_hidden_layers=num_hidden_layers, hidden_layers_width=hidden_layers_width, relu_fn=sqrelu_clamp)
+    def __init__(self, num_hidden_layers=0, hidden_layers_width=32, **kwargs):
+        super().__init__(num_hidden_layers=num_hidden_layers, hidden_layers_width=hidden_layers_width, relu_fn=sqrelu_clamp, **kwargs)
 
     def _num_king_buckets(self):
         return 1
@@ -175,9 +270,12 @@ class BasicNNUE(NNUEModel):
 
 def main(pgnfile, validation_pgn):
     print("Loading model")
-    n = BasicNNUE()
-    model = n.train(pgnfile, validation_pgn, profile=True, epochs=1)
+    n = BasicNNUE(centipawn_output=False)
+    model = n.train(pgnfile, validation_pgn, profile=False, epochs=10, batch_size=256)
     return model
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
+    # n = BasicNNUE(centipawn_output=False)
+    # for i in n.fast_result_iterator(sys.argv[1]):
+    #     print(i)
---
+++ net/trainlib.cc
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include "fenboard.hh"
+#include "pgn.hh"
+#include <stdlib.h>
+#include <random>
+
+const int RANDOM = 0;
+
+struct TrainingPosition {
+    uint64_t piece_bitmasks[12];
+    uint64_t piece_bitmasks_mirrored[12];
+    int white_king_index;
+    int black_king_index_mirrored;
+    float cp_eval;
+    float padding;
+};
+
+
+struct TrainingIterator {
+public:
+    TrainingIterator(const char *filename, int move_freq)
+        : input_stream(filename), distrib(1, move_freq), gen(rd())
+    {}
+    bool read_position(TrainingPosition *tp);
+private:
+    bool process_game(TrainingPosition *tp);
+
+    std::ifstream input_stream;
+    std::map<std::string, std::string> game_metadata;
+    std::vector<std::pair<move_annot, move_annot> > movelist;
+
+    unsigned int ply;
+    unsigned int next_ply;
+    std::uniform_int_distribution<> distrib;
+    Fenboard b;
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen; // mersenne_twister_engine seeded with rd()
+};
+
+extern "C" {
+
+TrainingIterator *create_training_iterator(const char *filename, int move_freq) {
+    TrainingIterator *ti = new TrainingIterator(filename, move_freq);
+    return ti;
+}
+bool read_position(TrainingIterator *iter, TrainingPosition *tp) {
+    return iter->read_position(tp);
+}
+
+void delete_training_iterator(TrainingIterator *ti) {
+    delete ti;
+}
+
+}
+
+bool TrainingIterator::read_position(TrainingPosition *tp)
+{
+    while (1) {
+        // game is too short or has no annotations: skip
+        if (movelist.size() > 3 && !movelist[0].first.eval.empty()) {
+            bool found_position = process_game(tp);
+            if (found_position) {
+                return true;
+            }
+        }
+
+        // finished game, start new one
+        game_metadata.clear();
+        movelist.clear();
+
+        if (input_stream.eof()) {
+            return false;
+        }
+        read_pgn(input_stream, game_metadata, movelist);
+        ply = 0;
+        b.set_starting_position();
+    }
+    return false;
+}
+
+int mirror_idx_vertical(int idx) {
+    return idx ^ 56;
+}
+
+uint64_t mirror_bitmap_vertical(uint64_t x) {
+    return  ( (x << 56)                           ) |
+            ( (x << 40) & 0x00ff000000000000ULL ) |
+            ( (x << 24) & 0x0000ff0000000000ULL ) |
+            ( (x <<  8) & 0x000000ff00000000ULL ) |
+            ( (x >>  8) & 0x00000000ff000000ULL ) |
+            ( (x >> 24) & 0x0000000000ff0000ULL ) |
+            ( (x >> 40) & 0x000000000000ff00ULL ) |
+            ( (x >> 56) );
+}
+
+bool TrainingIterator::process_game(TrainingPosition *tp)
+{
+    while (ply < movelist.size()) {
+        std::string move_text, next_move_text;
+        Color side_to_play = ply % 2 == 0 ? White : Black;
+        if (side_to_play == Black) {
+            move_text = movelist[ply / 2].second.move;
+            if ((ply / 2) + 1 >= movelist.size()) {
+                // this was the last move; might not be quiescent so don't use it
+                return false;
+            }
+            next_move_text = movelist[ply / 2 + 1].first.move;
+        } else {
+            move_text = movelist[ply / 2].first.move;
+            next_move_text = movelist[ply / 2].second.move;
+            if (next_move_text.empty()) {
+                // this was the last move; might not be quiescent so don't use it
+                return false;
+            }
+        }
+        move_t move = b.read_move(move_text, side_to_play);
+        b.apply_move(move);
+        // only consider quiescent moves as ones that aren't check nor result in capture/check
+        if (ply >= next_ply && move_text.find("+") == std::string::npos && next_move_text.find_first_of("+x") == std::string::npos) {
+            // exclude forced mates
+            std::string score = side_to_play == White ? movelist[ply/2].first.eval : movelist[ply/2].second.eval;
+            if (score.find('#') == std::string::npos) {
+                tp->cp_eval = std::stof(score);
+                for (int i = 0; i < 12; i++) {
+                    tp->piece_bitmasks[i] = b.piece_bitmasks[i + 1 + (i >= 6 ? 1 : 0)];
+                    tp->piece_bitmasks_mirrored[i >= 6 ? i - 6 : i + 6] = mirror_bitmap_vertical(b.piece_bitmasks[i + 1 + (i >= 6 ? 1 : 0)]);
+                }
+                tp->white_king_index = get_low_bit(b.piece_bitmasks[bb_king], 0);
+                tp->black_king_index_mirrored = mirror_idx_vertical(get_low_bit(b.piece_bitmasks[bb_king + bb_king + 1], 0));
+                ply++;
+                return true;
+            }
+        }
+        ply++;
+    }
+
+    return false;
+}
